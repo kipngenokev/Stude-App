1a4ec6433f4d7c184b2d1332feba3e6c9f956da0
kipngenokev <kkipngeno409@gmail.com>
2025-10-04 23:37:24 +0300

    creating a service and implementing it to save a student in the db

--- src/main/java/com/kipngeno/StudeApp/service/StudentService.java
+++ src/main/java/com/kipngeno/StudeApp/service/StudentService.java
@@ -1,4 +1,8 @@
 package com.kipngeno.StudeApp.service;
 
+import com.kipngeno.StudeApp.model.Student;
+
 public interface StudentService {
+
+    public Student saveStudent(Student student);
 }
--- src/main/java/com/kipngeno/StudeApp/service/StudentServiceImpl.java
+++ src/main/java/com/kipngeno/StudeApp/service/StudentServiceImpl.java
@@ -1,4 +1,21 @@
 package com.kipngeno.StudeApp.service;
 
-public class StudentServiceImpl {
+import com.kipngeno.StudeApp.model.Student;
+import com.kipngeno.StudeApp.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+   private final StudentRepository studentRepository;
+
+   public StudentServiceImpl(StudentRepository studentRepository) {
+       this.studentRepository = studentRepository;
+   }
+
+    @Override
+    public Student saveStudent(Student student) {
+         return studentRepository.save(student);
+    }
 }
