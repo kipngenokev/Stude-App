bd78c6bb32a09ac2bf0a648be43b86c8118819b1
kipngenokev <kkipngeno409@gmail.com>
2025-10-04 23:11:10 +0300

    created database configurations

--- src/main/java/com/kipngeno/StudeApp/repository/StudentRepository.java
+++ src/main/java/com/kipngeno/StudeApp/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.kipngeno.StudeApp.repository;
 
-public interface StudentRepository {
+import com.kipngeno.StudeApp.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
