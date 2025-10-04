daea6ad583aaa935a38f092353b58c5e839d9c88
kipngenokev <kkipngeno409@gmail.com>
2025-10-04 19:47:12 +0300

    setting up

---
+++ src/main/java/com/kipngeno/StudeApp/StudeAppApplication.java
@@ -0,0 +1,13 @@
+package com.kipngeno.StudeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kipngeno/StudeApp/StudeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kipngeno.StudeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
