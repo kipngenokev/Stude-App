3f2cab62b88e7d9b2b4bca581546e71bd607649a
kipngenokev <kkipngeno409@gmail.com>
2025-10-05 13:48:02 +0300

    updated frontend essentials,and created the AppBar

---
+++ studefrontend/src/App.js
@@ -0,0 +1,14 @@
+
+import AppBar from '@mui/material/AppBar';
+import './App.css';
+import CustomAppBar from './components/appBar';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomAppBar/>
+    </div>
+  );
+}
+
+export default App;
